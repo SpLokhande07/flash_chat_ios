06e0fd58bfe6053a00027517c7c96b5a946817ca
Saiprasad Lokhande <lokhandesai07@gmail.com>
2023-12-17 13:35:15 +0530

    basic chat done

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -15,9 +15,9 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var messageTextfield: UITextField!
     var db = Firestore.firestore()
     var messages : [Message] = [
-    Message(sender: "test1@gmail.com", body: "Hi"),
-    Message(sender: "test2@gmail.com", body: "Hello"),
-    Message(sender: "test1@gmail.com", body: "What's up?")
+//    Message(sender: "test1@gmail.com", body: "Hi"),
+//    Message(sender: "test2@gmail.com", body: "Hello"),
+//    Message(sender: "test1@gmail.com", body: "What's up?")
     ]
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,13 +40,14 @@ class ChatViewController: UIViewController {
         db.collection(K.FStore.collectionName)
             .order(by: K.FStore.dateField)
             .addSnapshotListener { querySnapshot, error in
+                self.messages = []
             if let e = error {
                 print("Something went wrong while retriving data \(e)")
             }
             else{
-                
+               
                 if let snapshotDocument = querySnapshot?.documents{
-                    self.messages = []
+                   
                     for doc in snapshotDocument {
                         let data = doc.data()
                         if let messageSender = data[K.FStore.senderField] as?  String , let messageBody = data[K.FStore.bodyField] as?  String {
@@ -54,7 +55,9 @@ class ChatViewController: UIViewController {
                             self.messages.append(newMessage)
                             DispatchQueue.main.async {
                                 self.tableView.reloadData()
-                            
+                                let indexPath = IndexPath(row: self.messages.count-1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+
                             }
                             
                         }
@@ -81,6 +84,10 @@ class ChatViewController: UIViewController {
                    print("There was an error \(e)")
                }else{
                    print("Successfully save data")
+                   DispatchQueue.main.async {
+                       self.messageTextfield.text = ""
+            
+                   }
                }
            }}
     }
@@ -96,9 +103,7 @@ class ChatViewController: UIViewController {
         
     }
 }
-
-
-
+ 
 extension ChatViewController : UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return messages.count
@@ -108,35 +113,25 @@ extension ChatViewController : UITableViewDataSource{
         let message = messages[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
         cell.label?.text = message.body
-        if message.sender == Auth.auth().currentUser?.email{
+        if message.sender == Auth.auth().currentUser?.email {
             cell.leftImageView.isHidden = true
             cell.rightImageView.isHidden = false
             cell.messageLabel.backgroundColor = UIColor(named: K.BrandColors.purple)
-            
             cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
-        }else{
-           
-           
-            
+        } else{
             cell.leftImageView.isHidden = false
             cell.rightImageView.isHidden = true
             cell.messageLabel.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
             cell.label.textColor = UIColor(named: K.BrandColors.purple)
             }
-        let indexPath = IndexPath(row: messages.count-1, section: 0)
-        self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+//        let indexPath = IndexPath(row: messages.count-1, section: 0)
+//        self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
 return cell
     }
-    
-    
 }
 
-
-
 extension ChatViewController : UITableViewDelegate {
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath.row)
     }
-    
 }
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -17,13 +17,20 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLabel.text = K.appName;
+        
         Auth.auth().addStateDidChangeListener(
             { auth, user in
                 let userData = user
-                if userData != nil {
-                    self.performSegue(withIdentifier: "IsAlreadyLoggedIn", sender: self)
-                }
+                print(userData?.uid)
+                print(auth.currentUser)
+                DispatchQueue.main.asyncAfter(deadline: .now()+1, execute: {
+                    if auth.currentUser != nil {
+                        self.performSegue(withIdentifier: "IsAlreadyLoggedIn", sender: self)
+                    }
+                })
+               
             }
                                             )
+        
     }
 }
